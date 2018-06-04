4517b76faaa26d2885c1ec6dab3a9318eeef5eb0
victorhundo <victorhundo@gmail.com>
2018-06-04 20:55:14 -0300

    Adicionado testes para getters e represtações

--- atgProj2/src/TestGraph.java
+++ atgProj2/src/TestGraph.java
@@ -12,29 +12,50 @@ public class TestGraph {
 		gPonderado.readGraph("./src/grafoPonderado.txt");
 	}
 	
+	@Test
+	public void testGetVertex() {
+		Assert.assertEquals(5, grafo.getVertexNumber(grafo.getGraph()));
+	}
+	
+	@Test
+	public void testGetEdges() {
+		Assert.assertEquals(5, grafo.getEdgeNumber(grafo.getGraph()));
+	}
+	
+	
+	@Test
+	public void testGetMeanEdge() {
+		Assert.assertEquals(2.0, grafo.getMeanEdge(grafo.getGraph()),0.0);
+	}
+	
 	 @Test
-	    public void testSimpleGraphRepresentations() {
+	    public void testSimpleGraphRepresentationsAL() {
 
-	        String expected = "1 - 2 5" + System.getProperty("line.separator") +
-	                "2 - 1 5" + System.getProperty("line.separator") +
-	                "3 - 5" + System.getProperty("line.separator") +
-	                "4 - 5" + System.getProperty("line.separator") +
-	                "5 - 1 2 3 4";
-
-	        String expectedAM =
-	                "    1   2   3   4   5" + System.getProperty("line.separator") +
-	                        "1   0   1   0   0   1" + System.getProperty("line.separator") +
-	                        "2   1   0   0   0   1" + System.getProperty("line.separator") +
-	                        "3   0   0   0   0   1" + System.getProperty("line.separator") +
-	                        "4   0   0   0   0   1" + System.getProperty("line.separator") +
-	                        "5   1   1   1   1   0" + System.getProperty("line.separator");
-
+	        String expected = 
+	        		"1 - 2 5 " + System.getProperty("line.separator") +
+	                "2 - 1 5 " + System.getProperty("line.separator") +
+	                "3 - 5 " + System.getProperty("line.separator") +
+	                "4 - 5 " + System.getProperty("line.separator") +
+	                "5 - 1 2 3 4 ";
 
 	        try {
 	            Assert.assertEquals(expected, grafo.graphRepresentation(grafo.getGraph(), RepresentationType.AL));
 	        } catch (Exception e) {
 	            Assert.fail();
 	        }
+	    }
+	 
+	 @Test
+	    public void testSimpleGraphRepresentationsAM() {
+
+	        String expectedAM =
+	                "0 1 2 3 4 5 " + System.getProperty("line.separator") +
+	                "1 0 1 0 0 1 " + System.getProperty("line.separator") +
+	                "2 1 0 0 0 1 " + System.getProperty("line.separator") +
+	                "3 0 0 0 0 1 " + System.getProperty("line.separator") +
+	                "4 0 0 0 0 1 " + System.getProperty("line.separator") +
+	                "5 1 1 1 1 0 " + System.getProperty("line.separator");
+
 	        try {
 	            Assert.assertEquals(expectedAM, grafo.graphRepresentation(grafo.getGraph(), RepresentationType.AM));
 	        } catch (Exception e) {
